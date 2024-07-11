a1c141fe6597e3e992c8a014f4e3d74f2a2a840b
Kirill Guzhev <dazaimycash@gmail.com>
2024-07-11 14:09:00 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,28 @@
-import operations.ManageNumb;
-import operations.ManageArrays;
-import operations.ManageStrings;
+import Employeers.Worker;
+import Entertainment.Park;
 
 public class Main {
-    public static void main(String[] args) {
-        int allArrays = 20;
-        final int eleArrayCount = 100;
-        int fiveTaskFirst = 5, fiveTaskSecond = 11;
-        int sixTaskNumber = 0;
-        int sevenTaskNumber = -150;
-        int eightTaskNumber = 10;
-        String eightTaskWord = "ASTON";
-        int nineTaskNumber = 1993;
-        int[] arrayTen = {0, 1, 0, 1, 1, 0};
-        int[] arrayEle = new int[eleArrayCount];
-        int[] arrayTwe = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        int arraySizeFou = 13;
-        int initialValueFou = 15;
+    public static void main(String[] args){
+    Worker[] workerArray = new Worker[5];
+    Worker workerTask = new Worker("Kira", "Junior", "Kira@gmail.com","+375256661111", 1000, 18);
+    workerArray[0] = workerTask;
+    workerArray[1] = new Worker("Alex", "Junior", "Alex@gmail.com", "+375253231613", 1000, 20);
+    workerArray[2] = new Worker("David", "Junior", "David@gmail.com", "+375257771215", 1000, 20);
+    workerArray[3] = new Worker("Max", "Beginner", "Max@gmail.com", "+375256123131", 1100, 28);
+    workerArray[4] = new Worker("Nikita", "Middle", "Nikita@gmail.com", "+375256091252", 1200, 30);
 
-        ManageStrings stringTasks = new ManageStrings();
-        stringTasks.printThreeWords();
-        ManageNumb numberTasks = new ManageNumb();
-        numberTasks.checkSumSign();
-        numberTasks.printColor();
-        numberTasks.compareNumbers();
-        numberTasks.workFiv(fiveTaskFirst, fiveTaskSecond);
-        numberTasks.workSix(sixTaskNumber);
-        numberTasks.workSev(sevenTaskNumber);
-        stringTasks.workEig(eightTaskWord, eightTaskNumber);
-        numberTasks.workNin(nineTaskNumber);
-        ManageArrays arrayTasks = new ManageArrays();
-        arrayTasks.workTen(arrayTen, allArrays);
-        arrayTasks.workEle(arrayEle, allArrays);
-        arrayTasks.workTwe(arrayTwe);
-        arrayTasks.workThi();
-        arrayTasks.workFou(arraySizeFou, initialValueFou);
+    Park park = new Park();
+    Park.Attraction[] attractionsArray = new Park.Attraction[3];
+    attractionsArray[0] = park.new Attraction("Башня Падения", "10:00 - 17:00", 20);
+    attractionsArray[1] = park.new Attraction("Седьмое Небо", "10:00 - 19:00", 15);
+    attractionsArray[2] = park.new Attraction("Космическое путешествие", "10:00 - 18:00", 25);
+
+    System.out.println("\nПервое задание:\n\n" + workerTask);
+    System.out.println("\n Второе задание:\n");
+    workerTask.allWorkers(workerArray);
+    System.out.println("\n Третье задание:\n");
+    park.attractionsInPark(attractionsArray);
+    park.attractionsInfo(attractionsArray);
+
     }
 }
-
