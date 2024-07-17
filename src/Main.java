02e453e0443fac0bd5458bbb84b989d8b28d9fe9
Kirill Guzhev <dazaimycash@gmail.com>
2024-07-17 14:25:42 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,38 @@
-import Employeers.Worker;
-import Entertainment.Park;
+import animal.Animal;
+import animal.Cat;
+import animal.Dog;
+import geometry.Circle;
+import geometry.Triangle;
+import geometry.Rectangle;
+import geometry.calculations.Shape;
+import tableware.Bowl;
+
+import java.awt.*;
 
 public class Main {
-    public static void main(String[] args){
-    Worker[] workerArray = new Worker[5];
-    Worker workerTask = new Worker("Kira", "Junior", "Kira@gmail.com","+375256661111", 1000, 18);
-    workerArray[0] = workerTask;
-    workerArray[1] = new Worker("Alex", "Junior", "Alex@gmail.com", "+375253231613", 1000, 20);
-    workerArray[2] = new Worker("David", "Junior", "David@gmail.com", "+375257771215", 1000, 20);
-    workerArray[3] = new Worker("Max", "Beginner", "Max@gmail.com", "+375256123131", 1100, 28);
-    workerArray[4] = new Worker("Nikita", "Middle", "Nikita@gmail.com", "+375256091252", 1200, 30);
-
-    Park park = new Park();
-    Park.Attraction[] attractionsArray = new Park.Attraction[3];
-    attractionsArray[0] = park.new Attraction("Башня Падения", "10:00 - 17:00", 20);
-    attractionsArray[1] = park.new Attraction("Седьмое Небо", "10:00 - 19:00", 15);
-    attractionsArray[2] = park.new Attraction("Космическое путешествие", "10:00 - 18:00", 25);
-
-    System.out.println("\nПервое задание:\n\n" + workerTask);
-    System.out.println("\nВторое задание:\n");
-    workerTask.allWorkers(workerArray);
-    System.out.println("\nТретье задание:\n");
-    park.attractionsInPark(attractionsArray);
-    park.attractionsInfo(attractionsArray);
-
+    public static void main(String[] args) {
+    Dog dogBobik = new Dog("Бобик");
+    Cat catMarkiz = new Cat("Маркиз");
+    Cat catKaido = new Cat("Кайдо");
+    Shape circle = new Circle("Круг",5, Color.RED, Color.BLACK);
+    Shape rectangle = new Rectangle("Прямоугольник",4, 6, Color.GREEN, Color.BLUE);
+    Shape triangle = new Triangle("Треугольник",3, 4, 5, Color.YELLOW, Color.MAGENTA);
+        System.out.println("\nПервое задание:");
+        dogBobik.run(150);
+        dogBobik.swim(5);
+        catMarkiz.run(150);
+        catMarkiz.swim(5);
+    Bowl bowl = new Bowl(0);
+        bowl.addFood(46);
+    Cat[] cats = {catMarkiz, catKaido};
+        catMarkiz.eat(bowl, 12);
+        catMarkiz.isTheCatFull(cats);
+        System.out.println("Всего животных: " + Animal.getAnimalCount());
+        System.out.println("Всего собак: " + Dog.getDogCount());
+        System.out.println("Всего котов: " + Cat.getCatCount());
+        System.out.println("\nВторое задание:");
+        circle.printShapeDetails();
+        rectangle.printShapeDetails();
+        triangle.printShapeDetails();
     }
 }
